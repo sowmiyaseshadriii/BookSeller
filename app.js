9f7b85cfee0105261f0b7521b793e58f6450be23
sowmiyaseshadri <sowmiyaseshadrii@gmail.com>
2018-04-01 01:50:33 +0800

    fixed keys file

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.get('/',(req,res)=>{
   res.render('index',{
-      stripePublisableKey:keys.stripePublishableKey
+      stripePublishableKey:keys.stripePublishableKey
   });
 });
 
